25c2041f299383856f76b07ac5e2609abf5cdd1f
httplogn <2350981823@qq.com>
2019-06-22 19:49:02 +0800

    测试更新

--- test2/src/main/java/com/lan/lgp/test/Test.java
+++ test2/src/main/java/com/lan/lgp/test/Test.java
@@ -3,6 +3,7 @@ package main.java.com.lan.lgp.test;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("hello world!");
 		System.out.println("hello github!");
 	}
 
